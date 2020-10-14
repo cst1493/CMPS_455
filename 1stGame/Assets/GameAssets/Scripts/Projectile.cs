bdcb9767bff4d2eb76f413e8943be6f524d8c440
fpbellow <fpbellow@gmail.com>
2020-10-14 17:24:27 -0500

    made the projectile clones be destroyed after a set time and refactored some folders

--- 1stGame/Assets/GameAssets/Scripts/Projectile.cs
+++ 1stGame/Assets/GameAssets/Scripts/Projectile.cs
@@ -1,13 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
     public float speed;
     public Rigidbody2D rigidBody;
+    public float lifetime = 5f;
     void Start()
     {
         rigidBody.velocity = transform.right * speed;
+        Destroy(gameObject, lifetime);
+
     }
 }
