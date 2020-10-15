36dbf93844686d49d412b3f463d5308eb842e938
DarrylMunsch <darrylmunsch@gmail.com>
2020-10-15 15:35:10 -0500

    Spawner script to spawn 10 spores with 5s delay. Currently only spawns black spores

---
+++ 1stGame/Assets/GameAssets/Scripts/SporeSpawner.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SporeSpawner : MonoBehaviour
+{
+    public float enemyCount = 10.0f;
+    private float spawnTime = 5.0f;
+
+    //
+    public GameObject SporeRed;
+    public GameObject SporeBlack;
+    public GameObject SporeGreen;
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnAction());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    IEnumerator SpawnAction()
+    {
+        while (enemyCount != 0)
+        {
+            Instantiate(SporeBlack, this.transform.position, this.transform.rotation);
+            enemyCount--;
+            yield return new WaitForSeconds(spawnTime);
+        }
+    }
+}
