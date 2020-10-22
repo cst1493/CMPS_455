943fcab3c2c21c88aa14061c4e1db923491c210e
Cameron <cst1493@gmail.com>
2020-10-21 22:59:10 -0500

    Added walls and projectiles destroy on collision.
    Feel free to overwrite anything in the sample scene.  It wasn't that many changes...  I took forever trying to make projectiles move through players, then realized I'm an idiot and noticed they were just hitting other projectiles because they were so big.

--- 1stGame/Assets/GameAssets/Scripts/Projectile.cs
+++ 1stGame/Assets/GameAssets/Scripts/Projectile.cs
@@ -11,8 +11,24 @@ public class Projectile : MonoBehaviour
 
     void Start()
     {
+        this.tag = "Player"; //projectiles can move through each other.
         rigidBody.velocity = transform.rotation * Vector3.right * speed;
         Destroy(gameObject, lifetime);
 
+    }
+
+    
+    private void OnTriggerEnter2D(Collider2D collider) //On"Collision"Enter2D collider if wanting to activate on collider instead of a trigger.
+    {
+        if ( collider.gameObject.tag != "Player" ) //move through players.
+        {
+            if (collider.gameObject.tag == "Enemy")
+            {
+                Debug.Log("Project hit an enemy");
+                //TODO deal damage to enemy.
+            }
+            Destroy(this.gameObject);
+            return;
+        } //Debug.Log("Projectile ignored something.");
     }
 }
