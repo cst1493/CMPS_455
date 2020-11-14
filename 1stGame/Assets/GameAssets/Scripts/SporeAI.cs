9400daf168458ba25d38311614f1b0af231fb608
Cameron <cst1493@gmail.com>
2020-11-14 00:17:49 -0600

    Level design and enemy AI finished.
    Enemy AI works and destroys itself on collision after dealing damage.
    If we want to do sounds, the main things would be in the SporeAI onCollision method when taking damage, and when firing a bullet in the player movement script.

--- 1stGame/Assets/GameAssets/Scripts/SporeAI.cs
+++ 1stGame/Assets/GameAssets/Scripts/SporeAI.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class SporeAI : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private GameObject closestPlayer;
+    float speed = 1.0f;
+    Vector2 currentPos;
     void Start()
     {
-        
+        closestPlayer = FindClosestPlayer();
+        currentPos = this.gameObject.transform.position;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        this.transform.position = currentPos;
+        currentPos = Vector2.MoveTowards(currentPos, closestPlayer.transform.position, (speed * Time.deltaTime));
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            //TODO make player damage sound if we have time.
+            GameObject.Destroy(gameObject);
+        }
+    }
+
+    public GameObject FindClosestPlayer() //https://docs.unity3d.com/ScriptReference/GameObject.FindGameObjectsWithTag.html
+    {
+        GameObject[] gos;
+        gos = GameObject.FindGameObjectsWithTag("Player");
+        GameObject closest = null;
+        float distance = Mathf.Infinity;
+        Vector3 position = transform.position;
+        foreach (GameObject go in gos)
+        {
+            Vector3 diff = go.transform.position - position;
+            float curDistance = diff.sqrMagnitude;
+            if (curDistance < distance)
+            {
+                closest = go;
+                distance = curDistance;
+            }
+        }
+        return closest;
     }
 }
